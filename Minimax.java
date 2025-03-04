8b891d049d65e27ec6132c43995739d50af1569d
Kevin <kegr@itu.dk>
2025-03-04 12:28:44 +0100

    Added a matrix that gives additional points to specific positions
    Co-authored-by:
    Olivia
    Stine
    Alberte

--- Minimax.java
+++ Minimax.java
@@ -7,9 +7,17 @@ import java.util.ArrayList;
 public class Minimax implements IOthelloAI{
 
     private static final int maxDepth = 5;
+    private float[][] penaltyMatrix;
     public Position decideMove(GameState s){
 
-        return minimax_search(s);
+        if (penaltyMatrix == null){
+            penaltyMatrix = GenerateMatrix(s.getBoard().length);
+        }
+        double timer = System.currentTimeMillis();
+        Position p = minimax_search(s);
+        System.out.println("Time taken: " + ((System.currentTimeMillis() - timer) / 1000));
+        return p;
+
     }
 
     private Position minimax_search(GameState state){
@@ -19,11 +27,9 @@ public class Minimax implements IOthelloAI{
 
     private Move max_value(GameState state, float alpha, float beta, int depth){
         if (state.legalMoves().isEmpty() || depth >= maxDepth){
-            System.out.println("End of line: " + state.countTokens()[state.getPlayerInTurn()-1]);
-            return new Move(null, state.countTokens()[state.getPlayerInTurn()-1]);
+            //return new Move(null, state.countTokens()[state.getPlayerInTurn()-1]);
+            return new Move(null, Evaluation(state.getBoard(), state.getPlayerInTurn()-1));
         }
-
-        System.out.println("Testing a move depth: " + depth);
 
         Move m = new Move(null, Float.MIN_VALUE);
 
@@ -36,7 +42,6 @@ public class Minimax implements IOthelloAI{
                 m.utility = m2.utility;
                 m.pos = pos;
                 alpha = Math.max(alpha, m.utility);
-                System.out.println(m);
             }
 
             if (m.utility >= beta){
@@ -48,7 +53,9 @@ public class Minimax implements IOthelloAI{
 
     private Move min_value(GameState state, float alpha, float beta, int depth){
         if (state.legalMoves().isEmpty() || depth >= maxDepth){
-            return new Move(null, state.countTokens()[state.getPlayerInTurn()-1]);
+
+            //return new Move(null, state.countTokens()[state.getPlayerInTurn()-1]);
+            return new Move(null, Evaluation(state.getBoard(), state.getPlayerInTurn()-1));
         }
 
         Move m = new Move(null, Float.MAX_VALUE);
@@ -58,7 +65,7 @@ public class Minimax implements IOthelloAI{
             if (state2 == null){
                 continue;
             }
-            Move m2 = max_value(state2, alpha, beta, depth++);
+            Move m2 = max_value(state2, alpha, beta, depth+1);
             if (m2.utility < m.utility) {
                 m.utility = m2.utility;
                 m.pos = pos;
@@ -82,7 +89,52 @@ public class Minimax implements IOthelloAI{
         return state2;
     }
 
-    
+    private float Evaluation(int[][] board, int player){
+        float sum = 0;
+        for (int i = 0; i < board.length; i++){
+            for (int y = 0; y < board.length; y++){
+                if (board[i][y] == player){
+                    sum += penaltyMatrix[i][y];
+                }
+            }
+        }
+
+        return sum;
+        //return penaltyMatrix[pos.col][pos.row];
+    }
+
+    private float[][] GenerateMatrix(int size){
+        float[][] matrix = new float[size][size];
+
+        for (int i = 0; i < size; i++){
+            for (int y = 0; y < size; y++){
+                matrix[i][y] = 1;
+            }
+        }
+
+        // Corners
+        matrix[0][0]            = 50f;
+        matrix[0][size-1]       = 50f;
+        matrix[size-1][0]       = 50f;
+        matrix[size-1][size-1]  = 50f;
+
+        // Around corners
+        matrix[1][0]            = -20f;
+        matrix[0][1]            = -20f;
+        matrix[size-1][size-2]  = -20f;
+        matrix[size-2][size-1]  = -20f;
+        matrix[0][size-2]       = -20f;
+        matrix[1][size-1]       = -20f;
+        matrix[size-1][1]       = -20f;
+        matrix[size-2][0]       = -20f;
+
+        // Around corners diagonally
+        matrix[1][1]            = -50f;
+        matrix[size-2][size-2]  = -50f;
+        matrix[size-2][1]       = -50f;
+        matrix[1][size-2]       = -50f;
+        return matrix;
+    }
 
 
 
