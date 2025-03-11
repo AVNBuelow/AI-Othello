4a4b572cf95261661fb8a3509bfcc7338af81b8e
Kevin <kegr@itu.dk>
2025-03-11 17:33:21 +0100

    Fixed the Evalutaion function
    Co-authored-by:
    Alberte
    Stine

--- OthelloAIStoak.java
+++ OthelloAIStoak.java
@@ -1,23 +1,21 @@
-import java.util.ArrayList;
 
 /***
  * This is implemented via the slides Lecture 3 slide 10
  */
 
-public class Minimax implements IOthelloAI{
+public class OthelloAIStoak implements IOthelloAI{
 
-    private static final int maxDepth = 5;
+    private static final int maxDepth = 10;
+    private static final boolean useMatrix = true;
     private float[][] penaltyMatrix;
     public Position decideMove(GameState s){
-
         if (penaltyMatrix == null){
             penaltyMatrix = GenerateMatrix(s.getBoard().length);
         }
         double timer = System.currentTimeMillis();
         Position p = minimax_search(s);
-        System.out.println("Time taken: " + ((System.currentTimeMillis() - timer) / 1000));
+        System.out.println("Time taken: " + ((System.currentTimeMillis() - timer) / 1000) + " to achieve p: " + p);
         return p;
-
     }
 
     private Position minimax_search(GameState state){
@@ -27,17 +25,22 @@ public class Minimax implements IOthelloAI{
 
     private Move max_value(GameState state, float alpha, float beta, int depth){
         if (state.legalMoves().isEmpty() || depth >= maxDepth){
-            //return new Move(null, state.countTokens()[state.getPlayerInTurn()-1]);
-            return new Move(null, Evaluation(state.getBoard(), state.getPlayerInTurn()-1));
+            if (useMatrix){
+                return new Move(null, Evaluation(state.getBoard(), state.getPlayerInTurn()-1));
+            }else{
+                return new Move(null, state.countTokens()[state.getPlayerInTurn()-1]);
+            }
         }
 
         Move m = new Move(null, Float.MIN_VALUE);
 
         for (Position pos : state.legalMoves()){
+            if (pos == null) { continue; }
+
             GameState state2 = Result(state, pos, state.getPlayerInTurn());
             if (state2 == null) { continue; }
             Move m2 = min_value(state2, alpha, beta, depth+1);
-            if (m2.utility > m.utility) {
+            if (m2.utility > m.utility || m2.pos == null) {
                 m.utility = m2.utility;
                 m.pos = pos;
                 alpha = Math.max(alpha, m.utility);
@@ -47,29 +50,31 @@ public class Minimax implements IOthelloAI{
                 return m;
             }
         }
+
         return m;
     }
 
     private Move min_value(GameState state, float alpha, float beta, int depth){
         if (state.legalMoves().isEmpty() || depth >= maxDepth){
-
-            //return new Move(null, state.countTokens()[state.getPlayerInTurn()-1]);
-            return new Move(null, Evaluation(state.getBoard(), state.getPlayerInTurn()-1));
+            if (useMatrix){
+                return new Move(null, Evaluation(state.getBoard(),state.getPlayerInTurn()-1));
+            }else{
+                return new Move(null, state.countTokens()[state.getPlayerInTurn()-1]);
+            }
         }
 
         Move m = new Move(null, Float.MAX_VALUE);
 
         for (Position pos : state.legalMoves()){
+            if (pos == null) {continue;}
             GameState state2 = Result(state, pos, state.getPlayerInTurn());
-            if (state2 == null){
-                continue;
-            }
+            if (state2 == null) { continue; }
+
             Move m2 = max_value(state2, alpha, beta, depth+1);
-            if (m2.utility < m.utility) {
+            if (m2.utility < m.utility || m2.pos == null) {
                 m.utility = m2.utility;
                 m.pos = pos;
                 beta = Math.min(beta, m.utility);
-
             }
 
             if (m.utility <= alpha){
@@ -99,7 +104,6 @@ public class Minimax implements IOthelloAI{
         }
 
         return sum;
-        //return penaltyMatrix[pos.col][pos.row];
     }
 
     private float[][] GenerateMatrix(int size){
@@ -107,34 +111,53 @@ public class Minimax implements IOthelloAI{
 
         for (int i = 0; i < size; i++){
             for (int y = 0; y < size; y++){
-                matrix[i][y] = 1;
+                matrix[i][y] = 50;
             }
         }
 
         // Corners
-        matrix[0][0]            = 50f;
-        matrix[0][size-1]       = 50f;
-        matrix[size-1][0]       = 50f;
-        matrix[size-1][size-1]  = 50f;
+        matrix[0][0]            = 100f;
+        matrix[0][size-1]       = 100f;
+        matrix[size-1][0]       = 100f;
+        matrix[size-1][size-1]  = 100f;
 
         // Around corners
-        matrix[1][0]            = -20f;
-        matrix[0][1]            = -20f;
-        matrix[size-1][size-2]  = -20f;
-        matrix[size-2][size-1]  = -20f;
-        matrix[0][size-2]       = -20f;
-        matrix[1][size-1]       = -20f;
-        matrix[size-1][1]       = -20f;
-        matrix[size-2][0]       = -20f;
+        matrix[1][0]            = 30f;
+        matrix[0][1]            = 30f;
+        matrix[size-1][size-2]  = 30f;
+        matrix[size-2][size-1]  = 30f;
+        matrix[0][size-2]       = 30f;
+        matrix[1][size-1]       = 30f;
+        matrix[size-1][1]       = 30f;
+        matrix[size-2][0]       = 30f;
 
         // Around corners diagonally
-        matrix[1][1]            = -50f;
-        matrix[size-2][size-2]  = -50f;
-        matrix[size-2][1]       = -50f;
-        matrix[1][size-2]       = -50f;
+        matrix[1][1]            = 1f;
+        matrix[size-2][size-2]  = 1f;
+        matrix[size-2][1]       = 1f;
+        matrix[1][size-2]       = 1f;
+
+        // Prints Matrix in terminal
+        PrintBoard(matrix);
+
         return matrix;
     }
 
-
-
+    
+    private void PrintBoard(float[][] board){
+        int size = board.length;
+        for (int i = 0; i < size; i++){
+            for (int y = 0; y < size; y++){
+                System.out.print(board[i][y]);
+                for (int space = 0; space < (7 - String.valueOf(board[i][y]).length()); space++){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            for (int line = 0; line < (size * 7); line++){
+                System.out.print("_");
+            }
+            System.out.println();
+        }
+    }
 }
